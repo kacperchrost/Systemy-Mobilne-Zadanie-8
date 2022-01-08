3892c44a461a758ce89f15f72ed4a821dd37f328
Kacper <66553710+kacperchrost@users.noreply.github.com>
2022-01-08 12:00:23 +0100

    1

---
+++ app/src/main/java/com/example/myapplication/SensorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SensorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sensor_activity)
+    }
+}
